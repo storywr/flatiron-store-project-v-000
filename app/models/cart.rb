7f1d1670603d42857e1e4f3082289682196adaea
Bill <storywr@gmail.com>
2017-02-01 14:33:37 -0500

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,17 @@
 class Cart < ActiveRecord::Base
   belongs_to :user
+  has_many :line_items
   has_many :items, through: :line_items
+
+  def total
+    total = 0
+    self.items.each do |item|
+      total += item.price
+    end
+    total
+  end
 
   def add_item
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_many :carts
+
+  def current_cart
+    current_cart = Cart.find(session[:cart_id])
+  end
+
 end
